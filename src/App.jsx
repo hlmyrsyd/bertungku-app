294613d8a629b60e0114f0ac2d01d6f74857f019
jodyboy <elite.crew66@gmail.com>
2024-04-29 14:27:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigations'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <header>
+        <h1>Welcome to Bertungku</h1>
+      </header>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigations.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Navigations.css'
+
+function Navigation() {
+    return (
+        <nav className='navigation'>
+            <div className='logo'>
+                <img src="src\assets\bertungku-logo.png" alt="bertungku logo" />
+            </div>
+            <ul className="nav-links">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+            </ul>
+            <div className="book-now">
+                <a href="#book-now" className="btn">Book Now</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
